d8417bc4f36692ef112b0b1e0ffc91726de4bfcb
Baron <nastakoz28@gmail.com>
2025-04-19 16:15:41 +0300

    Update pin_pong.py

--- pin_pong.py
+++ pin_pong.py
@@ -11,10 +11,42 @@ class Game(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+class Bone(Game):
+    def update(self, speed):
+        key_pressed = key.get_pressed()
+        if key_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= speed
+        if key_pressed[K_DOWN] and self.rect.y < 460:
+            self.rect.y += speed
+    def left_update(self, speed):
+        key_pressed = key.get_pressed()
+        if key_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= speed
+        if key_pressed[K_s] and self.rect.y < 460:
+            self.rect.y += speed
 
-window = display.set_mode((900, 600))
+x_speed = 2
+y_speed = 2
+class Ball(Game):
+    def update(self, x_speed, y_speed):
+        self.rect.x += x_speed
+        self.rect.y += y_speed
+        if self.rect.y < 0:
+            y_speed *= -1
+        if self.rect.x > 1200 or self.rect.x <= 80:
+            x_speed *= -1
+        '''if sprite.spritecollide(bone_right, ball , False):
+            x_speed *= -1'''
+
+window = display.set_mode((1100, 700))
 display.set_caption('Пин-понг')
-background = transform.scale(image.load('background.jpg'),(900,600))
+background = transform.scale(image.load('background.jpg'),(1100, 700))
+
+bone_right = Bone('bone.png', 1000, 200, 5, 40, 240)
+bone_left = Bone('bone.png', 60, 200, 5, 40, 240)
+
+ball = Ball('ball.png', 550, 1, 5, 80, 80)
+platform =  Bone('bone.png', 0, 700, 0, 1100, 10)
 
 finish = False
 game = True
@@ -26,6 +58,26 @@ while game:
             game = False
     if finish != True:
         window.blit(background, (0,0))
+        bone_right.reset()
+        bone_right.update(5)
+
+        bone_left.reset()
+        bone_left.left_update(5)
+
+        platform.reset()
+
+        ball.reset()
+        ball.rect.x += x_speed
+        ball.rect.y += y_speed
+        if ball.rect.y <=  0:
+            y_speed *= -1
+        if ball.rect.x >= 1200 or ball.rect.x <= 80:
+            x_speed *= -1
+        if ball.rect.y >= 620:
+            y_speed *= -1
+        if ball.rect.colliderect(bone_right.rect) or ball.rect.colliderect(bone_left.rect):
+            x_speed *= -1
+
 
     display.update()
     clock.tick(FPS)
